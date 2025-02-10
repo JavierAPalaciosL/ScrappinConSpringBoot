d0df41b0a935b895979ed9ab5cecdf5b3eff00a2
JavierAPalaciosL <javi.261280@gmail.com>
2025-02-10 00:06:05 -0600

    Primer commit bien hecho

---
+++ src/main/java/com/musica/letras/LetrrasApplication.java
@@ -0,0 +1,13 @@
+package com.musica.letras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetrrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetrrasApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/musica/letras/controller/Lyrics.java
@@ -0,0 +1,24 @@
+package com.musica.letras.controller;
+
+import com.musica.letras.util.Minecraft;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class Lyrics {
+
+    @PostMapping("/lyrics")
+    public ResponseEntity<HashMap<Object, Object>> lyrics(@RequestParam String artist, @RequestParam String title) {
+        Minecraft mc = new Minecraft();
+        mc.setArtist(artist);
+        mc.setTitle(title);
+        return ResponseEntity.ok(new HashMap<>() {{ put("letter ", mc.getLetter());}});
+    }
+
+
+
+}
