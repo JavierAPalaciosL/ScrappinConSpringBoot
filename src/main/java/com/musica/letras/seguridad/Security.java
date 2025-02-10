770a3729a315730f66bae2f5e109490019bf83af
JavierAPalaciosL <javi.261280@gmail.com>
2025-02-10 01:12:24 -0600

    commit de rutas protegidas

---
+++ src/main/java/com/musica/letras/seguridad/Security.java
@@ -0,0 +1,26 @@
+package com.musica.letras.seguridad;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(httpSecurityCsrfConfigurer ->
+        {
+            httpSecurityCsrfConfigurer.disable();
+        }).authorizeHttpRequests(authorizeRequests ->{
+            authorizeRequests.requestMatchers("/users/login").permitAll()
+                    .requestMatchers("/users/register").permitAll()
+                        .anyRequest().authenticated();
+                })
+                .build();
+    }
+
+}
