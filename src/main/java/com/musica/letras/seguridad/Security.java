7523c6e7d86e5a977b97651ea5d627201dd47ecd
JavierAPalaciosL <javi.261280@gmail.com>
2025-02-10 11:26:55 -0600

    Ahora si edito lo que genera un conflicto

--- src/main/java/com/musica/letras/seguridad/Security.java
+++ src/main/java/com/musica/letras/seguridad/Security.java
@@ -18,7 +18,7 @@ public class Security {
         }).authorizeHttpRequests(authorizeRequests ->{
             authorizeRequests.requestMatchers("/users/login").permitAll()
                     .requestMatchers("/users/register").permitAll()
-                    .requestMatchers("/users/conflicto").permitAll()
+                    .requestMatchers("/users/conflcto").permitAll()
                         .anyRequest().authenticated();
             //EDITANDO EL MISMO ARCHIVO PARA VER SI EXISTEN CONFLICTOS
 
