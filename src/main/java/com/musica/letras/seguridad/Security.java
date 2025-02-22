011738b715ad32753f07130ba4fb851d87f39e8b
Javier423 <trollmaster383@gmail.com>
2025-02-22 05:55:53 +0100

    salvando el sout

--- src/main/java/com/musica/letras/seguridad/Security.java
+++ src/main/java/com/musica/letras/seguridad/Security.java
@@ -42,7 +42,7 @@ public class Security {
 
     private String web(){
         System.out.println("la web");
-        System.out.println("la deep web");
+        System.out.println("la de web");
 
         return "web";
     }
